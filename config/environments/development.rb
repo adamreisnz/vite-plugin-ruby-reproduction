0f013ccd6070ae41a7bc026638b94e386718abcf
Adam Reis <adam@reis.nz>
2024-09-14 13:02:22 +1200

    Update development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,24 +1,5 @@
 # frozen_string_literal: true
 
-require 'active_support/core_ext/integer/time'
-
 Rails.application.configure do
-  # Settings specified here will take precedence over those in config/application.rb.
 
-  config.hosts << ENV['WEB_HOST_NAME'] if ENV['WEB_HOST_NAME'].present?
-
-  # In the development environment your application's code is reloaded any time
-  # it changes. This slows down response time but is perfect for development
-  # since you don't have to restart the web server when you make code changes.
-  config.cache_classes = false
-
-  # Do not eager load code on boot.
-  config.eager_load = true
-
-  # Show full error reports.
-  config.consider_all_requests_local = true
-
-  # Use an evented file watcher to asynchronously detect changes in source code,
-  # routes, locales, etc. This feature depends on the listen gem.
-  config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 end
