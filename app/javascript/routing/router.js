6ab24a4a27acbd6fc43e89bfe40bcc1b0e0d2ca1
Adam Reis <adam@reis.nz>
2024-10-15 10:04:41 +1300

    Fix issue

--- app/javascript/routing/router.js
+++ app/javascript/routing/router.js
@@ -2,5 +2,5 @@ import {createRouter, createWebHistory} from 'vue-router'
 import {routes} from './routes.js'
 
 //Create history and router
-export const history = createWebHistory(import.meta.env.BASE_URL)
+export const history = createWebHistory()
 export const router = createRouter({history, routes})
